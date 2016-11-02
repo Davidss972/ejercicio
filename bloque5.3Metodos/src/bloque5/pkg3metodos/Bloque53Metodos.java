a4c492e3d9c96641b5540e59f7221541e95478b3
Davidss972 <davidss97@gmail.com>
2016-11-02 11:30:27 +0100

    Metodos
    Ejemplo en el que creamos un metodo alternativo, para poner los
    atributos en privado y seguir llamandolos desde la clase main mediante
    "funciones" o "metodos".

---
+++ bloque5.3Metodos/src/bloque5/pkg3metodos/Bloque53Metodos.java
@@ -0,0 +1,15 @@
+
+package bloque5.pkg3metodos;
+//instanciar significa la creacion de un objeto a partir de la clase
+public class Bloque53Metodos {
+
+    public static void main(String[] args) 
+    {
+        Guerrero g1 = new Guerrero();
+        
+        g1.setNombre("goku");//lo que ponga entreparentesis
+        System.out.println(g1.getNombre());
+        
+    }
+    
+}
